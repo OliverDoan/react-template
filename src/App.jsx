e500784c25a55a01bf5b9a69e0d107c40da3f230
Oliver Doan <doanvietthuan1005@gmail.com>
2022-08-23 16:54:28 +0700

    add ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
-import './assets/scss/styles.scss'
-function App() {
+import './index.css'
+// import components
+import Header from './components/Header'
+import Hero from './components/Hero'
+import Brands from './components/Brands'
+import About from './components/About'
+import Portfolio from './components/Portfolio'
+import Skills from './components/Skills'
+import Services from './components/Services'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+import Testimonials from './components/Testimonials'
+import BackTopBtn from './components/BackTopBtn'
+
+const App = () => {
   return (
-    <>
-      <h1>Hello React</h1>
-    </>
+    <div className='bg-white relative'>
+      <Header />
+      <Hero />
+      {/* <Brands /> */}
+      <About />
+      <Skills />
+      <Portfolio />
+      <Services />
+      <Testimonials />
+      <Contact />
+      <Footer />
+      <BackTopBtn />
+    </div>
   )
 }
 
